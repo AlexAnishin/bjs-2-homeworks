528ca9fc90a08700d2a40115f135cd4823a1525d
AlexAnishin <sinka_6799@mail.ru>
2022-08-24 10:45:03 +0300

    Домашнее задание к лекции 2. Функции Задача 1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,44 @@
-// Задание 1
+"use strict";
+
 function getArrayParams(arr) {
-  let min, max, sum, avg;
-
-  // Ваш код
-
-  return { min: min, max: max, avg: avg };
+    let min, max, sum, avg;
+    min = Infinity;
+    max = -Infinity;
+    sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (min > arr[i]) {
+            min = arr[i];
+        }
+        if (max < arr[i]) {
+            max = arr[i];
+        }
+        sum = sum + arr[i];
+    }
+    let floatAvg = sum / arr.length;
+    let stringAvg = floatAvg.toFixed(2);
+    avg = Number.parseFloat(stringAvg);
+    return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+    let sum;
 
-  // Ваш код
+    // Ваш код
 
-  return sum;
+    return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+    let max;
 
-  // Ваш кода
-  // for ...
-  
-  return max;
+    // Ваш кода
+    // for ...
+
+    return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+    // Ваш код
 }
