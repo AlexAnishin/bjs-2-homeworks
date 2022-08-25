a8f5556bc643d009741cbfaeae3a4cea5d27371f
AlexAnishin <sinka_6799@mail.ru>
2022-08-25 11:44:38 +0300

    Домашнее задание к лекции 3. Массивы задача 1

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,24 @@
+"use strict";
+
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+    let result;
+    result = true;
+    if (arr1.length !== arr2.length) {
+        result = false;
+    } else {
+        for (let i = 0; i < arr1.length; i++) {
+            if (arr1[i] !== arr2[i]) {
+                result = false;
+            }
+        }
+    }
+    return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
+    let resultArr;
 
-  // Ваш код
+    // Ваш код
 
-  return resultArr; // array
+    return resultArr; // array
 }
