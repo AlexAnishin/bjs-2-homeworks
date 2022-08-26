91b3160db60dc0ccb4fcce77865e73f7b023a4ee
AlexAnishin <sinka_6799@mail.ru>
2022-08-26 11:47:19 +0300

    Домашнее задание к лекции 4 Объекты

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,41 @@
+"use strict";
+
 function Student(name, gender, age) {
-    // Ваш код
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+}
+
+Student.prototype.setSubject = function(subjectName) {
+    this.subject = subjectName;
 
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+Student.prototype.addMark = function(mark) {
+    if (this.marks === undefined) {
+        this.marks = [mark];
+    } else {
+        this.marks.push(mark);
+    }
 }
 
-// ваш код для остальных методов
+Student.prototype.addMarks = function() {
+    if (this.marks === undefined) {
+        this.marks = [];
+    }
+    for (let i = 0; i < arguments.length; i++) {
+        this.marks.push(arguments[i]);
+    }
+}
+Student.prototype.getAverage = function() {
+    if (this.marks === undefined || this.marks.length === 0) {
+        return 0;
+    } else {
+        return this.marks.reduce((acc, el) => acc + el, 0) / this.marks.length;
+    }
+}
+Student.prototype.exclude = function(reason) {
+    this.excluded = reason;
+    delete this.marks;
+    delete this.subject;
+}
