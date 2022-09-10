91728debe52b46cb774cf269712e85a37271a801
AlexAnishin <sinka_6799@mail.ru>
2022-09-10 22:00:18 +0300

    Домашнее задание к лекции 7 Асинхронность

--- 7.async/task.js
+++ 7.async/task.js
@@ -28,6 +28,68 @@ class AlarmClock {
         } else {
             return false
         }
-
     }
+    getCurrentFormattedTime() {
+        var d = new Date();
+        return d.toLocaleTimeString().substr(0, 5);
+    }
+    checkClock() {
+        this.alarmCollection.forEach((alarm) => {
+            if (alarm.time === this.getCurrentFormattedTime()) {
+                alarm.callback()
+            }
+        })
+    }
+    start() {
+        this.checkClock()
+        this.timerId = setInterval(() => {
+            this.checkClock()
+        }, 1000)
+    }
+    stop() {
+        if (this.timerId) {
+            clearInterval(this.timerId);
+            this.timerId = null
+        }
+    }
+    printAlarms() {
+        this.alarmCollection.forEach((alarm) => {
+            console.log(alarm.id, alarm.time)
+        })
+    }
+    clearAlarms() {
+        this.stop();
+        this.alarmCollection = [];
+    }
+
+}
+const testCase = () => {
+    let alarmClock = new AlarmClock();
+    console.log(alarmClock);
+    let currentTime = new Date();
+    alarmClock.addClock(
+        currentTime.toLocaleTimeString().substr(0, 5),
+        () => {
+            setInterval(() => {
+                console.log("вызывается несколько раз")
+            }, 500)
+        },
+        1
+    )
+    alarmClock.addClock(
+        `${currentTime.getHours()}:${parseInt(currentTime.getMinutes())+1}`,
+        () => {
+            console.log("+1")
+        },
+        2
+    )
+    alarmClock.addClock(
+        `${currentTime.getHours()}:${parseInt(currentTime.getMinutes())+2}`,
+        () => {
+            console.log("+2")
+        },
+        3
+    )
+    alarmClock.printAlarms();
+    alarmClock.start();
 }
