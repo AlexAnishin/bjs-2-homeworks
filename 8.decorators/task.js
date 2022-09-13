fa9aae022f8e5211c156d5ac66ec02a3359bbf1f
AlexAnishin <sinka_6799@mail.ru>
2022-09-13 12:15:40 +0300

    задача 1 . дикораторы

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,26 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+    const cache = {}
+    let indecies = [];
+    return (...args) => {
+        const hash = args.join(',');
+        if (cache.length >= 5) {
+            const kee = indecies.shift();
+            delete(cache[kee])
+        }
+        let result = cache[hash];
+        if (result === undefined) {
+            result = cache[hash] = func(...args);
+            indecies.push(hash);
+            output = "Вычисляем: " + result;
+        } else {
+            output = "Из кэша: " + result;
+        }
+        console.log(output);
+        return output;
+    }
 }
 
 
 function debounceDecoratorNew(func) {
-  // Ваш код
+    // Ваш код
 }
